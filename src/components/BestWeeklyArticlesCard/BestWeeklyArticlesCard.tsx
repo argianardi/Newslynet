37e96799037b5fbb46028499873f0adb394e6764
Argian Ardi Prasetya <argianardi1435@gmail.com>
2023-03-28 04:22:01 +0700

    feat: Add link to article detail page

--- src/components/BestWeeklyArticlesCard/BestWeeklyArticlesCard.tsx
+++ src/components/BestWeeklyArticlesCard/BestWeeklyArticlesCard.tsx
@@ -1,42 +1,49 @@
 import { FC } from 'react';
 import { Card } from 'antd';
+import { Link } from 'react-router-dom';
 
 import { Article } from '../../types/article';
 
 interface Props {
   article: Article;
+  onGotoArticleDetail: () => void;
 }
 
-const BestWeeklyArticlesCard: FC<Props> = ({ article }) => {
+const BestWeeklyArticlesCard: FC<Props> = ({
+  article,
+  onGotoArticleDetail,
+}) => {
   return (
-    <Card hoverable style={{ marginBottom: 20 }}>
-      <div style={{ display: 'flex', alignItems: 'center' }}>
-        <div style={{ marginRight: 5 }}>
-          <p className="date" style={{ marginBottom: 2, fontSize: 11 }}>
-            {article.publishedAt}
-          </p>
-          <h3
-            className="title"
-            style={{
-              fontSize: 12,
-              marginBottom: 4,
-            }}
-          >
-            {article.title}
-          </h3>
-          <a className="link" style={{ marginBottom: 0 }}>
-            Read more...
-          </a>
+    <Link to={`/article/detail/${article.title}`} onClick={onGotoArticleDetail}>
+      <Card hoverable style={{ marginBottom: 20 }}>
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+          <div style={{ marginRight: 5 }}>
+            <p className="date" style={{ marginBottom: 2, fontSize: 11 }}>
+              {article.publishedAt}
+            </p>
+            <h3
+              className="title"
+              style={{
+                fontSize: 12,
+                marginBottom: 4,
+              }}
+            >
+              {article.title}
+            </h3>
+            <a className="link" style={{ marginBottom: 0 }}>
+              Read more...
+            </a>
+          </div>
+          <div style={{ width: 300, height: 120 }}>
+            <img
+              alt={article.title}
+              src={article.urlToImage}
+              style={{ objectFit: 'cover', width: '100%', height: '100%' }}
+            />
+          </div>
         </div>
-        <div style={{ width: 300, height: 120 }}>
-          <img
-            alt={article.title}
-            src={article.urlToImage}
-            style={{ objectFit: 'cover', width: '100%', height: '100%' }}
-          />
-        </div>
-      </div>
-    </Card>
+      </Card>
+    </Link>
   );
 };
 
--- src/components/CategoryCard/CategoryCard.tsx
+++ src/components/CategoryCard/CategoryCard.tsx
@@ -1,26 +1,30 @@
 import { Card } from 'antd';
 import { FC } from 'react';
+import { Link } from 'react-router-dom';
 
 import { Article } from '../../types/article';
 
 interface Props {
   article: Article;
+  onGotoArticleDetail: () => void;
 }
 
-const CategoryCard: FC<Props> = ({ article }) => {
+const CategoryCard: FC<Props> = ({ article, onGotoArticleDetail }) => {
   return (
-    <Card hoverable style={{ marginBottom: 20 }}>
-      <p style={{ fontSize: 11 }} className="date">
-        {article.publishedAt}
-      </p>
-      <h2 className="title" style={{ fontSize: 14 }}>
-        {article.title}
-      </h2>
-      <p className="desc">{article.description}</p>
-      <a className="link" href={article.url}>
-        Read more...
-      </a>
-    </Card>
+    <Link to={`/article/detail/${article.title}`}>
+      <Card hoverable style={{ marginBottom: 20 }}>
+        <p style={{ fontSize: 11 }} className="date">
+          {article.publishedAt}
+        </p>
+        <h2 className="title" style={{ fontSize: 14 }}>
+          {article.title}
+        </h2>
+        <p className="desc">{article.description}</p>
+        <a className="link" href={article.url}>
+          Read more...
+        </a>
+      </Card>
+    </Link>
   );
 };
 
