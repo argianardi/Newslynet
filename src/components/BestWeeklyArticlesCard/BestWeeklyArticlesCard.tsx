12ea02fc7c45d1a386513244cb2d5465a9099d24
Argian Ardi Prasetya <argianardi1435@gmail.com>
2023-03-28 23:36:02 +0700

    refactor: reorganize code to handle adding and removing articles on news archive page

--- src/components/BestWeeklyArticlesCard/BestWeeklyArticlesCard.tsx
+++ src/components/BestWeeklyArticlesCard/BestWeeklyArticlesCard.tsx
@@ -1,5 +1,5 @@
 import { FC } from 'react';
-import { Card } from 'antd';
+import { Card, Popconfirm } from 'antd';
 import moment from 'moment';
 
 import { Article } from '../../types/article';
@@ -7,15 +7,32 @@ import { Article } from '../../types/article';
 interface Props {
   article: Article;
   onGotoArticleDetail: () => void;
+  onAddToArchive: () => void;
+  onRemoveFromArchive: () => void;
+  isArchived: boolean;
 }
 
 const BestWeeklyArticlesCard: FC<Props> = ({
   article,
   onGotoArticleDetail,
+  onAddToArchive,
+  onRemoveFromArchive,
+  isArchived,
 }) => {
+  const handleAddToArchive = () => {
+    onAddToArchive();
+  };
+
+  const handleRemoveFromArchive = () => {
+    onRemoveFromArchive();
+  };
+
   return (
-    <Card onClick={onGotoArticleDetail} hoverable style={{ marginBottom: 20 }}>
-      <div style={{ display: 'flex', alignItems: 'center' }}>
+    <Card hoverable style={{ marginBottom: 20 }}>
+      <div
+        onClick={onGotoArticleDetail}
+        style={{ display: 'flex', alignItems: 'center' }}
+      >
         <div style={{ marginRight: 5 }}>
           <p className="date" style={{ marginBottom: 2, fontSize: 11 }}>
             {moment(article.publishedAt).format('MMMM DD, YYYY')}
@@ -29,17 +46,40 @@ const BestWeeklyArticlesCard: FC<Props> = ({
           >
             {article.title}
           </h3>
-          <p className="link" style={{ marginBottom: 0 }}>
-            Read more...
-          </p>
         </div>
-        <div style={{ width: 300, height: 120 }}>
-          <img
-            alt={article.title}
-            src={article.urlToImage}
-            style={{ objectFit: 'cover', width: '100%', height: '100%' }}
-          />
-        </div>
+        {article.urlToImage && (
+          <div style={{ width: 300, height: 120 }}>
+            <img
+              alt={article.title}
+              src={article.urlToImage}
+              style={{ objectFit: 'cover', width: '100%', height: '100%' }}
+            />
+          </div>
+        )}
+      </div>
+      <div style={{ marginBottom: -15 }}>
+        {!isArchived && (
+          <Popconfirm
+            title="Are you sure to add this article to archive?"
+            onConfirm={handleAddToArchive}
+            okText="Yes"
+            cancelText="No"
+          >
+            <button className="link" style={{ marginRight: 10 }}>
+              Add to archive
+            </button>
+          </Popconfirm>
+        )}
+        {isArchived && (
+          <Popconfirm
+            title="Are you sure to remove this article from archive?"
+            onConfirm={handleRemoveFromArchive}
+            okText="Yes"
+            cancelText="No"
+          >
+            <button className="link">Remove from archive</button>
+          </Popconfirm>
+        )}
       </div>
     </Card>
   );
--- src/components/CategoryCard/CategoryCard.tsx
+++ src/components/CategoryCard/CategoryCard.tsx
@@ -1,4 +1,4 @@
-import { Card } from 'antd';
+import { Card, Popconfirm } from 'antd';
 import { FC } from 'react';
 import moment from 'moment';
 
@@ -7,19 +7,61 @@ import { Article } from '../../types/article';
 interface Props {
   article: Article;
   onGotoArticleDetail: () => void;
+  onAddToArchive: () => void;
+  onRemoveFromArchive: () => void;
+  isArchived: boolean;
 }
 
-const CategoryCard: FC<Props> = ({ article, onGotoArticleDetail }) => {
+const CategoryCard: FC<Props> = ({
+  article,
+  onGotoArticleDetail,
+  onAddToArchive,
+  onRemoveFromArchive,
+  isArchived,
+}) => {
+  const handleAddToArchive = () => {
+    onAddToArchive();
+  };
+
+  const handleRemoveFromArchive = () => {
+    onRemoveFromArchive();
+  };
+
   return (
-    <Card onClick={onGotoArticleDetail} hoverable style={{ marginBottom: 20 }}>
-      <p style={{ fontSize: 11 }} className="date">
-        {moment(article.publishedAt).format('MMMM DD, YYYY')}
-      </p>
-      <h2 className="title" style={{ fontSize: 14 }}>
-        {article.title}
-      </h2>
-      <p className="desc">{article.description}</p>
-      <p className="link">Read more...</p>
+    <Card hoverable style={{ marginBottom: 20 }}>
+      <div onClick={onGotoArticleDetail} style={{ paddingBottom: 10 }}>
+        <p style={{ fontSize: 11 }} className="date">
+          {moment(article.publishedAt).format('MMMM DD, YYYY')}
+        </p>
+        <h2 className="title" style={{ fontSize: 14 }}>
+          {article.title}
+        </h2>
+        <p className="desc">{article.description}</p>
+      </div>
+      <div style={{ marginBottom: -10 }}>
+        {!isArchived && (
+          <Popconfirm
+            title="Are you sure to add this article to archive?"
+            onConfirm={handleAddToArchive}
+            okText="Yes"
+            cancelText="No"
+          >
+            <button className="link" style={{ marginRight: 10 }}>
+              Add to archive
+            </button>
+          </Popconfirm>
+        )}
+        {isArchived && (
+          <Popconfirm
+            title="Are you sure to remove this article from archive?"
+            onConfirm={handleRemoveFromArchive}
+            okText="Yes"
+            cancelText="No"
+          >
+            <button className="link">Remove from archive</button>
+          </Popconfirm>
+        )}
+      </div>
     </Card>
   );
 };
