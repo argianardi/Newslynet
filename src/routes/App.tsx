297f25964fcccb64eb721e46c1dc5430b2a2c3e9
Argian Ardi Prasetya <argianardi1435@gmail.com>
2023-03-28 07:13:55 +0700

    feat: Add router for article detail page

--- src/routes/App.tsx
+++ src/routes/App.tsx
@@ -3,12 +3,17 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import '../styles/App.css';
 
 import Home from '../pages/Home';
+import ArticleDetail from '../pages/ArticleDetail';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route
+          path="/article/detail/:articleTitle"
+          element={<ArticleDetail />}
+        />
       </Routes>
     </BrowserRouter>
   );
